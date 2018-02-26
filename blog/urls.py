c93b2e437f49e14b760aba747db763c4186d8a26
Monica <monica.babaian@gmail.com>
2018-02-25 19:01:16 -0600

    Added view and template for detailed blog post as well as CSS for site

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,20 @@
+# from django.conf.urls import url
+# from . import views
+# from django.shortcuts import render, get_object_or_404
+
+# urlpatterns = [
+#     url(r'^$', views.post_list, name='post_list'),
+#     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+# ]
+
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/post_detail.html', {'post': post})
+
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
